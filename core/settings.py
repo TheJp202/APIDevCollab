1981c59d0ddc61a8351d69e63f9fe4e7a5630f7d
TheJp202 <jeanpierrellc.jpllc@gmail.com>
2024-09-14 19:39:29 -0500

    render

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['apidevcollab.onrender.com']
 
 # Application definition
 
