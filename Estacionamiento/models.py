8841c1eb47b5f48193da63279178ba64b9610691
TheJp202 <jeanpierrellc.jpllc@gmail.com>
2024-09-15 19:07:00 -0500

    maps

--- Estacionamiento/models.py
+++ Estacionamiento/models.py
@@ -4,6 +4,8 @@ from datetime import date
 class Estacionamiento(models.Model):
     Nombre = models.CharField(max_length=50)
     Direccion = models.CharField(max_length=128,unique=True)
+    CoordenadaX = models.CharField(max_length=50)
+    CoordenadaY = models.CharField(max_length=50)
     FechaRegistro = models.DateField(default=date.today)
 
     def __str__(self):
